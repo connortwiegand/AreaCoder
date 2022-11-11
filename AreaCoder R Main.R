8284f50c1f6612b1d8e1db09150a511444405c35
Connor W <connortwiegand@gmail.com>
2022-11-11 15:12:47 -0800

    Initial commit of project and files

---
+++ AreaCoder R Main.R
@@ -0,0 +1,5 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, dplyr, magrittr, ggplot2)
+# Must install through github:
+# Even if plotting with ggplot2, contains curve_intersect function
+getwd("~/School/EC 327 Fa22")
