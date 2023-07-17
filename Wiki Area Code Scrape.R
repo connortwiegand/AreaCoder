120ffa08ddc496c9b4d0e27cb20b1273c4a31c4a
Connor Wiegand <connortwiegand@gmail.com>
2023-07-17 16:30:19 -0700

    added (distorted) peru map

--- Wiki Area Code Scrape.R
+++ Wiki Area Code Scrape.R
@@ -11,34 +11,8 @@ pacman::p_load(stringdist)
 
 # ---------------------------------------------------------------------
 
-### Longer Remarks:
+### Examples
 
-#* Also may need to try the following, in general (see assignment):
-#wiki_text <- html_text(wiki_wp)
-#substr(wiki_text, 1, 1000)
-
-# ---------------------------------------------------------------------
-
-# 1. Start by trying Peru:
-## (a) Get data:
-
-wiki_site <- read_html("https://en.wikipedia.org/wiki/Telephone_numbers_in_Peru")
-
-# Have to copy the xpath via inspect element 
-## How to automate in general?
-###
-wiki_xpath <- '/html/body/div[3]/div[3]/div[5]/div[1]/table[2]/tbody'
-## Here, using just the table body (no headers) is what works
-### headers are still preserved
-
-wiki_wp <- html_element(wiki_site, xpath = wiki_xpath) #*
-wiki_table <- html_table(wiki_wp)
-
-#So there that is...
-
-# -----------------
-
-##(b) get regions of Peru
 countries <- ne_countries(returnclass = "sf") %>% st_transform(8857) 
 
 data(admin1.map)
@@ -51,16 +25,12 @@ data(df_japan_census)
 df_japan_census$value = df_japan_census$pop_density_km2_2010
 admin1_choropleth("japan", df_japan_census)
 
+# ---------------------------------------------------------------------
 
-
-# centered at input data for low distortion
-
-?html_text2
 ### Ukraine:
 
 ua_wiki <- read_html(
   "https://en.wikipedia.org/wiki/List_of_dialling_codes_in_Ukraine")
-ua_xp <- '/html/body/div[3]/div[3]/div[5]/div[1]'
 ua_he <- html_elements(ua_wiki, css = "h3 .mw-headline")
 
 #Oblasts:
@@ -98,4 +68,54 @@ ua_df <- copy
 ggplot(get_admin1_map("ukraine"), aes(long, lat, color=region)) + 
   geom_path(aes(group=group), show.legend = F) +
   geom_label(data=ua_df, aes(long, lat, label=value, color=region), show.legend = F) +
+  ggdark::dark_theme_void()
+
+
+# ------------------------------------------------------------------------ 
+### Peru:
+pe_site <- read_html("https://en.wikipedia.org/wiki/Telephone_numbers_in_Peru")
+
+pe_wp <- html_elements(pe_site, css = " .wikitable") #*
+wiki_table <- html_table(pe_wp)
+pe_df <- wiki_table[[1]]
+pe_df %<>% rbind(c(1, "Callao"))
+pe_df[1,2] <- "Lima"
+for(i in 1:nrow(pe_df)) {
+  pe_df[i,2] <- paste0("region de ", tolower(pe_df[i,2])) 
+}
+pe_df[pe_df[,2]=="region de moquegua",2] <- "departamento de moquegua"
+pe_df %<>% rbind(c(1, "provincia de lima")) 
+names(pe_df) %<>% tolower()
+names(pe_df)[names(pe_df) == "area code"] <- "value"
+
+admin_pe <- get_admin1_regions("peru")[,2]
+
+admin_spots <- stringdist::amatch(admin_pe, unlist(pe_df[,2]), maxDist = "10")
+
+pe_df[,2]%<>% unlist()
+
+copy <- pe_df
+copy[,2] <- admin_pe[admin_spots]
+
+#missing <- setdiff(admin_pe, unlist(copy[,2]))
+#copy[which(copy[,1]  %>% is.na()),1] <- missing
+
+pe_df[,2] <- copy[,2]
+
+copy <- aggregate(cbind(long, lat) ~ region, 
+                  data=get_admin1_map("peru"),
+                  FUN=function(x)mean(range(x)))
+
+copy <- merge(pe_df, copy, by="region")
+# copy[copy$value==48,]$long %<>% sum(0.5)
+# copy[copy$value==48,]$lat %<>% sum(0.25)
+# copy[copy$value==44,]$lat %<>% sum(-0.25)
+# copy[copy$value==45,]$lat %<>% sum(0.1)
+# copy[copy$value==45,]$long %<>% sum(-0.1)
+
+pe_df <- copy
+
+ggplot(get_admin1_map("peru"), aes(long, lat, color=region)) + 
+  geom_path(aes(group=group), show.legend = F) +
+  geom_label(data=pe_df, aes(long, lat, label=value, color=region), show.legend = F) +
   ggdark::dark_theme_void()
